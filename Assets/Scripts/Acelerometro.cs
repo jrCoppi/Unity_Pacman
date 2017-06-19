3a3881569f6c75b3a30bc0a82ad5d4a1b9c750d9
Junior Coppi <jr.coppi@gmail.com>
2017-06-18 22:31:36 -0300

    Aceleromêtro funcional
    Aceleromêtro funcional

--- Assets/Scripts/Acelerometro.cs
+++ Assets/Scripts/Acelerometro.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Acelerometro : MonoBehaviour {
 
+	private double valorMarcador = 0.30;
+
 	private static Acelerometro acelerometroInicial;
 	public static Acelerometro Instance
 	{
@@ -20,8 +22,23 @@ public class Acelerometro : MonoBehaviour {
 
 	}
 
-	// Update is called once per frame
-	public void atualiza () {
-		
+	//Retorna a direção atual
+	public int getDirecao () {
+		int direcao = 0;
+		float aceleracao = Input.acceleration.x;
+
+		if (aceleracao > 0.30)
+		{
+			//Esquerda
+			direcao = 1;
+		}
+
+		if (aceleracao < -0.30)
+		{
+			//Direita
+			direcao = 2;
+		}
+
+		return direcao;
 	}
 }
